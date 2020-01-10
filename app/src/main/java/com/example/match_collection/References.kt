111b330330c9d1663b385015e5f5c7033c003324
Emily Lin <32380328+EmilyTLin@users.noreply.github.com>
2020-01-09 20:20:42 -0800

    Update References.kt to match match_collection_qr_schema.yml. (#27)

--- app/src/main/java/com/example/match_collection/References.kt
+++ app/src/main/java/com/example/match_collection/References.kt
@@ -10,25 +10,18 @@ package com.example.match_collection
 
 // Class to store information to be used to create the final match information map.
 class References {
-    // Team number variables.
-    var teamNumberOne: Int = 0
-    var teamNumberTwo: Int = 0
-    var teamNumberThree: Int = 0
-
-    // Match information variables.
+    // Data that is shared between the objective and subjective QRs.
+    lateinit var scout_name: String
+    var match_number: Int = 0
     // TODO ALLIANCE COLOR
-    var matchNumber: Int = 0
-    lateinit var scoutName: String
 
-    // Pre-match collection variables.
+    // Data specific to objective match collection QR.
+    var teamNumber: Int = 0
     // TODO STARTING LOCATION
-
-    // During match timeline collection variables.
+    var is_no_show: Boolean = false
     lateinit var timeline: ArrayList<HashMap<String, Any>>
 
     // Subjective relative data collection variables.
-    var speedRanking: Int = 0
-    var agilityRanking: Int = 0
-
-    lateinit var matchInformation: HashMap<String, Any>
+    lateinit var speed_rankings: List<Int>
+    lateinit var agility_rankings: List<Int>
 }
